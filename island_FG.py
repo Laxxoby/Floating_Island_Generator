63019642d861c3b79dbae665936ec9f6da0de33e
Camilo Lasso <laxxoby.j7@gmail.com>
2023-12-28 14:18:59 -0500

    ModeSemiU updated

--- island_FG.py
+++ island_FG.py
@@ -68,7 +68,7 @@ def generar_plataforma():
                 caja1 = Cubo([i + 0.5 , j + 0.5, altura + 0.5])
                 cubo = Poly3DCollection(caja1.caras, alpha=0.25, facecolors='green')
                 ax.add_collection3d(cubo)
-                artura = AlturaIsla(distancia, radio, altura, i, j)    
+                artura = AlturaIsla(distancia, radio, altura, i, j, modeIsland)    
                 ArrAltura[i, j] = artura        
     crear_cuadrados_svg(ArrAltura)
                 
@@ -87,24 +87,42 @@ def generar_plataforma():
     # Mostrar el gráfico
     plt.show()
 
-def AlturaIsla(distancia, radio, altura, i, j):
+def AlturaIsla(distancia, radio, altura, i, j, modeIsland):
+    """
+    Calcula la altura de cada cubo en función de su distancia al centro de la isla, y genera la representación gráfica en 3D.
+
+    Parámetros:
+    distancia -- Distancia al centro de la isla.
+    radio -- Radio del círculo que define la isla.
+    altura -- Altura máxima que puede tener la isla.
+    i -- Coordenada x del cubo.
+    j -- Coordenada y del cubo.
+
+    Retorna:
+    Cubos_altura -- Altura del cubo generado.
+
+    La función calcula la altura del cubo dependiendo de la distancia proporcionada y genera un conjunto de cubos en representación gráfica en 3D.
+    Además, agrega un texto en la parte superior de algunos cubos mostrando la cantidad de cubos en esa posición.
+    """
     global ax 
-    por = ((abs(sqrt(distancia) - radio) )/ radio ) * 100
-    if por <= 100 and por > 80:
-        Cubos_altura = random.randint(1, altura)
-    if por <= 80 and por > 60:
-        Cubos_altura = random.randint(1, int(altura * 0.8))
-    if por <= 60 and por > 40:
-        Cubos_altura = random.randint(1, int(altura * 0.6))
-    if por <= 40 and por > 20:
-        Cubos_altura = random.randint(1, int(altura * 0.4))
-    if por <= 20 and por > 0:
-        Cubos_altura = random.randint(1, int(altura * 0.2))
+    porcentaje = ((abs(sqrt(distancia) - radio)) / radio) * 100
+
+    if porcentaje <= 100 and porcentaje > 80:
+        Cubos_altura = random.randint(1 if modeIsland == "random" else int(altura * 0.8), altura)
+    elif porcentaje <= 80 and porcentaje > 60:
+        Cubos_altura = random.randint(1 if modeIsland == "random" else int(altura * 0.6), int(altura * 0.8))
+    elif porcentaje <= 60 and porcentaje > 40:
+        Cubos_altura = random.randint(1 if modeIsland == "random" else int(altura * 0.4), int(altura * 0.6))
+    elif porcentaje <= 40 and porcentaje > 20:
+        Cubos_altura = random.randint(1 if modeIsland == "random" else int(altura * 0.2), int(altura * 0.4))
+    elif porcentaje <= 20 and porcentaje > 0:
+        Cubos_altura = random.randint(1 , int(altura * 0.2))
         ax.text(i + 0.5, j + 0.5, altura + 1.5, str(Cubos_altura), color='red', fontsize=10, ha='center')
+
     for z in range(Cubos_altura + 1):
-        caja = Cubo([i + 0.5 , j + 0.5, (altura - 0.5) - z])
+        caja = Cubo([i + 0.5, j + 0.5, (altura - 0.5) - z])
         cubo = Poly3DCollection(caja.caras, alpha=0.25, facecolors='cyan' if z % 2 == 0 else 'blue')
-        ax.add_collection3d(cubo)  
+        ax.add_collection3d(cubo)
 
     return Cubos_altura
     
@@ -146,7 +164,7 @@ def validar_numero(mensaje, min_valor=1):
 
 def validar_modo():
     while True:
-        modo = input("Ingresa el modo en el que se creará la Isla Flotante (Random o SemiUniforme): ").lower()
+        modo = input("Ingresa el modo en el que se creará la Isla Flotante (random o semiUniforme): ").lower()
         if modo in ["random", "semiuniforme"]:
             return modo
         print("Por favor, ingresa 'random' o 'semiUniforme'.")
